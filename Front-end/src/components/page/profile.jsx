dc90f2890b14d189061bd947a9f8b27a750f9fe5
FrienD <90751306+FrienZz@users.noreply.github.com>
2024-02-08 18:20:11 +0700

    add frontend for testbackend api

---
+++ Front-end/src/components/page/profile.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
+export default function Profile() {
+  const [fullName, setFullName] = useState("");
+  const [cmuAccount, setCmuAccount] = useState("");
+  const [studentId, setStudentId] = useState("");
+  const [organizationName, setOrganizationName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/basicInfo", {
+        params: {
+          token: Cookies.get("cmu-oauth-token"),
+        },
+      })
+      .then((response) => {
+        if (response.data.ok) {
+          setFullName(response.data.firstName + " " + response.data.lastName);
+          setCmuAccount(response.data.cmuAccount);
+          setStudentId(response.data.studentId ?? "No Student Id");
+          setOrganizationName(response.data.organizationName);
+        }
+      })
+      .catch((error) => {
+        if (!error.response) {
+          setErrorMessage(
+            "Cannot connect to the network. Please try again later."
+          );
+        } else if (error.response.status === 401) {
+          setErrorMessage("Authentication failed");
+        } else if (error.response.data.ok === false) {
+          setErrorMessage(error.response.data.message);
+        } else {
+          setErrorMessage("Unknown error occurred. Please try again later");
+        }
+      });
+  }, []);
+
+  return (
+    <div className="p-3">
+      <h1>Hi, {fullName}</h1>
+      <p>{cmuAccount}</p>
+      <p>{studentId}</p>
+      <p>{organizationName}</p>
+      <p className="text-danger">{errorMessage}</p>
+    </div>
+  );
+}
