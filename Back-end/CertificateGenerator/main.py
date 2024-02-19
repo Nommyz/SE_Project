7386629c8818ea213a7cbd28c00ccffdcf4eddbf
Nommyz <phumiphat_sr@cmu.ac.th>
2024-02-19 15:14:07 +0700

    Create certificate generator

---
+++ Back-end/CertificateGenerator/main.py
@@ -0,0 +1,37 @@
+from PIL import Image, ImageFont, ImageDraw
+
+# Global Variables
+FONT_FILE = ImageFont.truetype(r'font/Lato-Italic.ttf', 120) #change the font here
+FONT_COLOR = "#000000"
+template_dir = 'template/template2.png'
+
+template = Image.open(template_dir)
+WIDTH, HEIGHT = template.size
+
+def make_certificates(name):
+    '''Function to save certificates as a .png file'''
+
+    image_source = Image.open(template_dir)
+    draw = ImageDraw.Draw(image_source)
+
+    # Finding the width of the text 
+    name_width = draw.textlength(name, font=FONT_FILE)
+
+    # Placeholder for name_height (or calculate differently if needed)
+    name_height = 100  
+
+    # Placing it in the center, then making some adjustments if somewhere else set value here.
+    draw.text(((WIDTH - name_width) / 2, (HEIGHT - name_height) / 2 - 30), name, fill=FONT_COLOR, font=FONT_FILE)
+
+    # Saving the certificates in a different directory.
+    image_source.save("./out/" + name +".png")
+    print('Saving Certificate of:', name)        
+
+if __name__ == "__main__":
+
+    names = ['Mr.Ohm', "Full Name"]
+    for name in names:
+        make_certificates(name)
+
+    print(len(names), "certificates done.")
+
