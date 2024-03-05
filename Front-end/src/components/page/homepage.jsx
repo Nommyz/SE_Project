b59e5ad899f4a76b85a45203aa16f6b4d0b9ce9e
FrienDDDD <friendjunk@gmail.com>
2024-03-06 04:00:16 +0700

    design homepage ui

--- Front-end/src/components/page/homepage.jsx
+++ Front-end/src/components/page/homepage.jsx
@@ -2,8 +2,7 @@ import "./sign-in.css";
 
 export default function Homepage() {
   return (
-    <div className="p-3 vstack gap-3">
-      <h1>Sign-in using CMU OAuth Example</h1>
+    <div className="login-btn">
       <a href="http://localhost:3000/login">
         <button className="sign-in">Sign-in with CMU Account</button>
       </a>
