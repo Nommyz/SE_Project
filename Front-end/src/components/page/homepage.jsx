fa912a5dea4f3d9d6be04311f98ae41f8ac38d4b
Thank29 <natsuphat_th@cmu.ac.th>
2024-03-06 17:29:44 +0700

    update homepage not finish

--- Front-end/src/components/page/homepage.jsx
+++ Front-end/src/components/page/homepage.jsx
@@ -2,10 +2,31 @@ import "./sign-in.css";
 
 export default function Homepage() {
   return (
-    <div className="login-btn">
-      <a href="http://localhost:3000/login">
-        <button className="sign-in">Sign-in with CMU Account</button>
-      </a>
+    <div>
+          <h1 style={{
+            fontFamily: "Arial, Helvetica, sans-serif",
+            fontSize: "5rem",
+            letterSpacing: "1.6rem",
+            wordSpacing:"0px",
+            color: "#000000" ,
+            marginTop:"250px",
+            fontWeight:"bolder",
+            animation :"myAnim"
+          }}>
+            ACTREC
+          </h1>
+          <p style={{
+            fontFamily: "Arial, Helvetica, sans-serif",
+            fontSize: "1rem",
+            letterSpacing: "0.34rem",
+            wordSpacing:"0px",
+            color: "#000000" ,
+          }}>
+            Connect Activitys , Skills , Achievements
+          </p>
+        <a href="http://localhost:3000/login">
+          <button className="sign-in">Sign-in with CMU Account</button>
+        </a>
     </div>
   );
 }
